a76dec7db3f992c098bf35fefd3e7371bf869543
Alfonso Scornavacca <alfondrums@gmail.com>
2022-08-23 07:51:41 -0300

    Cards component created

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,10 +1,18 @@
 import './Main.css'
 import FigureExample from './ProfileImage';
+import Cards from './Cards';
+import Button from 'react-bootstrap/Button';
 function Main () {
 
     return ( 
             <div className="main">
                 <FigureExample />
+                <div id='cards-container'>
+                    <Cards/>
+                    <Cards/>
+                    <Cards/>
+                </div>
+                <Button variant="primary">Go somewhere</Button>
             </div>
      );
 }
